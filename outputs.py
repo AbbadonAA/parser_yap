a536d53e737c1c530b0fd35581f5647aac98d125
Pushkarev Anton <pushkarevantona@gmail.com>
2022-08-04 00:06:46 +0300

    v.0.0.2

--- outputs.py
+++ outputs.py
@@ -1,11 +1,30 @@
 from prettytable import PrettyTable
+from constants import BASE_DIR, DATETIME_FORMAT
+import datetime as dt
+import csv
 
 
 def control_output(results, cli_args):
-    if cli_args.pretty:
+    output = cli_args.output
+    if output == 'pretty':
         pretty_output(results)
+    elif output == 'file':
+        file_output(results, cli_args)
     else:
         default_output(results)
+
+
+def file_output(results, cli_args):
+    results_dir = BASE_DIR / 'results'
+    results_dir.mkdir(exist_ok=True)
+    parser_mode = cli_args.mode
+    now = dt.datetime.now()
+    now_formatted = now.strftime(DATETIME_FORMAT)
+    file_name = f'{parser_mode}_{now_formatted}.csv'
+    file_path = results_dir / file_name
+    with open(file_path, 'w', encoding='utf-8') as file:
+        writer = csv.writer(file, dialect='unix')
+        writer.writerows(results)
 
 
 def pretty_output(results):
